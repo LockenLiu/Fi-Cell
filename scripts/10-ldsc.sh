05a841669c7d65324738cbbd903fd5169fdedc4f
Liu Junhui <103775557+LockenLiu@users.noreply.github.com>
2025-01-18 16:32:23 +0800

    Add files via upload

---
+++ scripts/10-ldsc.sh
@@ -0,0 +1,18 @@
+outdir=$1
+baselinemodeldir=$2
+cell=$3
+celllddir=$4
+trait=$5
+traitgwassumstatsdir=$6
+
+mkdir ${outdir}/res
+
+python ./ldsc/ldsc.py \
+--h2 ${traitgwassumstatsdir} \
+--ref-ld-chr ${baselinemodeldir}/baseline.,${celllddir}/${cell}. \
+--out ${outdir}/res/${cell}-${trait} \
+--overlap-annot  \
+--frqfile-chr ./data/ldsc/1000G_Phase3_frq/1000G.EUR.QC. \
+--w-ld-chr ./data/ldsc/1000G_Phase3_weights_hm3_no_MHC/weights.hm3_noMHC. \
+--print-coefficients  \
+--print-delete-vals  
