9589c60230f7c73f8ec740f495724f97ff460102
Liu Junhui <103775557+LockenLiu@users.noreply.github.com>
2025-01-18 12:40:23 +0800

    Update 10-ldsc.sh

--- scripts/10-ldsc.sh
+++ scripts/10-ldsc.sh
@@ -11,20 +11,10 @@ mkdir ${outdir}/res
 
 python ./ldsc/ldsc.py \
 --h2 ${traitgwassumstatsdir} \
---ref-ld-chr ${baselinemodeldir}/baseline.,${conditionlddir}/${conditionprefix}.,${celllddir}/${cell}. \
+--ref-ld-chr ${baselinemodeldir}/baseline.,${celllddir}/${cell}. \
 --out ${outdir}/res/${cell}-${trait} \
 --overlap-annot  \
 --frqfile-chr ./data/ldsc/1000G_Phase3_frq/1000G.EUR.QC. \
 --w-ld-chr ./data/ldsc/1000G_Phase3_weights_hm3_no_MHC/weights.hm3_noMHC. \
 --print-coefficients  \
 --print-delete-vals  
-
-# python ./ldsc/ldsc.py \
-# --h2 ${traitgwassumstatsdir} \
-# --ref-ld-chr ${baselinemodeldir}/baseline.,${celllddir}/${cell}. \
-# --out ${outdir}/res/${cell}-${trait} \
-# --overlap-annot  \
-# --frqfile-chr ./data/ldsc/1000G_Phase3_frq/1000G.EUR.QC. \
-# --w-ld-chr ./data/ldsc/1000G_Phase3_weights_hm3_no_MHC/weights.hm3_noMHC. \
-# --print-coefficients  \
-# --print-delete-vals  
